8a5d49f8308fa315557ce236f6f860e2d1ca47e1
Jurre van Eijk <jurreveijk13@gmail.com>
2022-08-26 21:07:13 +0200

    Add all features

--- script.js
+++ script.js
@@ -1,14 +1,56 @@
-function getComputerChoice()  {
-    let randomChoice = Math.floor(Math.random() * 3);
-    
-    if (randomChoice === 0) {
-        return "Rock";
-      } if (randomChoice === 1) {
-        return "Paper";
-      } if (randomChoice === 2) {
-        return "Scissors";
-      }
+function getComputerChoice() {
+  let compChoices = ["rock", "paper", "scissors"] // let computer pick [rock, paper, scissors]
+  let randomizeChoice = Math.floor(Math.random() * compChoices.length) + 1 // randomize rock paper and scissors
+ 
+  if (randomizeChoice === 1) {
+    return "rock";
+  } else if (randomizeChoice === 2) {
+    return "paper";
+  } else if (randomizeChoice === 3) {
+    return "scissors";
+  }
+ }
+ 
+function playSingleRound(getComputerChoice, playerSelection) {
+  if (getComputerChoice === playerSelection) {
+      return "its a tie!"
+  }
+  else if (playerSelection == "rock")
+      if (getComputerChoice == "paper")
+      return "youve lost";
+  
+  else {
+      return "player wins!"
+  }
+
+  else if (playerSelection == "paper")
+  if (getComputerChoice == "scissors")
+  return "youve lost";
+
+  else {
+      return "player wins!"
+  }
+
+  else if (playerSelection == "scissors")
+      if (getComputerChoice == "rock")
+      return "youve lost";
+
+  else {
+      return "player wins!"
+  }
+ 
+ }
+ 
+const computerChoice = getComputerChoice();
+console.log("Computer's Choice: ")
+console.log(computerChoice);
+let userInput = "paper"
+let playerSelection = userInput.toLowerCase();
+
+function game() {
+  for (let i = 0; i < 5; i++)  {
+      console.log(playSingleRound(getComputerChoice() , playerSelection))
+  }
 }
-console.log(getComputerChoice());
 
-
+game();
